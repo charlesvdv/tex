3919ca1783ce75d6c554cfe15587ddc11deca874
Charles Vandevoorde <charles.vandevoorde@hotmail.be>
2017-06-13 21:17:12 +0200

    improve clarity in the parser models

--- src/parser/models.rs
+++ src/parser/models.rs
@@ -1,13 +1,34 @@
+use lexer;
+
+/// Represents an element returned by the parser.
 #[derive(Debug)]
 pub enum ParserElem {
     Text(String),
     Command(Command),
 }
 
+/// Represents TeX primitives that we don't interpret.
 #[derive(Debug)]
 pub struct Command {
-    name: String,
-    parameter: Option<Vec<MacroDefParam>>,
+    pub name: String,
+    pub parameter: Option<Vec<ParserElem>>,
+}
+
+/// Represents a definition of a macro.
+pub struct MacroDefinition<'a> {
+    pub name: String,
+    pub parameters: Option<Vec<MacroDefParam>>,
+    pub body: MacroDefBody<'a>,
+}
+
+/// A macro can be defined by two ways:
+///
+/// - `def` where the macro is evaluated when the macro is called (Raw)
+/// - `edef` where macro is evaluated at definition (Expanded)
+#[derive(Debug)]
+pub enum MacroDefBody<'a> {
+    Expanded(ParserElem),
+    Raw(lexer::Elem<'a>),
 }
 
 #[derive(Debug)]
