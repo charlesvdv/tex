6530b995a4cf84a11f8acc9d852c1967b657d712
charlesvdv <charles.vandevoorde@hotmail.be>
2017-06-21 17:43:21 +0200

    add initial scopes (aka groups) handling

--- src/parser/context.rs
+++ src/parser/context.rs
@@ -1,7 +1,19 @@
-pub struct Context {}
+use parser::scope::Groups;
+
+pub struct Context {
+    groups: Groups,
+}
 
 impl Context {
     pub fn new() -> Self {
-        Context {}
+        Context { groups: Groups::new() }
+    }
+
+    pub fn groups(&self) -> &Groups {
+        &self.groups
+    }
+
+    pub fn groups_mut(&mut self) -> &mut Groups {
+        &mut self.groups
     }
 }
---
+++ src/parser/scope.rs
@@ -0,0 +1,78 @@
+use lexer::Catcodes;
+
+/// Handle TeX groups.
+pub struct Groups {
+    default: DefaultScope,
+    stack: Vec<ScopeHolder>,
+}
+
+impl Groups {
+    pub fn new() -> Self {
+        Groups {
+            default: DefaultScope::Normal,
+            // Add the global scope when creating the stack.
+            stack: vec![ScopeHolder::new()],
+        }
+    }
+
+    pub fn set_default_scope(&mut self, default_scope: DefaultScope) {
+        self.default = default_scope;
+    }
+
+    pub fn reset_default_scope(&mut self) {
+        self.default = DefaultScope::Normal;
+    }
+
+    pub fn get_default_scope(&self) -> &DefaultScope {
+        &self.default
+    }
+
+    pub fn get_catcode(&self) -> Catcodes {
+        self.stack.last().unwrap().catcodes.clone()
+    }
+
+    pub fn set_catcode(&mut self, code: usize, value: char) {
+        for scope in self.get_scopes() {
+            scope.set_catcode(code, value);
+        }
+    }
+
+    /// Get all the scopes that we need to change according to the current default scope.
+    fn get_scopes(&mut self) -> Vec<&mut ScopeHolder> {
+        match self.default {
+            // Should be safe to unwrap because we can't have a state where the global scope
+            // is not present.
+            DefaultScope::Normal => vec![self.stack.last_mut().unwrap()],
+            DefaultScope::Outer => {
+                // Check if we only have the global scope or not.
+                if self.stack.len() < 2 {
+                    return vec![self.stack.last_mut().unwrap()];
+                } else {
+                    self.stack.iter_mut().rev().take(2).collect()
+                }
+            }
+            DefaultScope::Global => self.stack.iter_mut().collect(),
+        }
+    }
+}
+
+pub enum DefaultScope {
+    Global,
+    Outer,
+    Normal,
+}
+
+/// Hold information about one scope.
+struct ScopeHolder {
+    catcodes: Catcodes,
+}
+
+impl ScopeHolder {
+    pub fn new() -> ScopeHolder {
+        ScopeHolder { catcodes: Catcodes::default() }
+    }
+
+    fn set_catcode(&mut self, code: usize, value: char) {
+        self.catcodes.set_catcode(code, value);
+    }
+}
