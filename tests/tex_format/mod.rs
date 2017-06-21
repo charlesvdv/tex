b43e0e86576cf8897781a6540dca1c712d75defa
charlesvdv <charles.vandevoorde@hotmail.be>
2017-06-21 14:33:21 +0200

    handle escaped char

--- tests/tex_format/mod.rs
+++ tests/tex_format/mod.rs
@@ -8,11 +8,42 @@ pub fn format(parser_output: &Vec<TeXToken>) -> String {
     for elem in parser_output {
         match elem {
             &TeXToken::Text(ref v) => {
-                out.push_str(&v);
+                out.push_str(&escape_str(v));
             }
             _ => continue,
         }
     }
     out.push_str("\\bye");
     out
+}
+
+fn escape_str(seq: &str) -> String {
+    let escaped_ch = [
+        ('&', "\\&"),
+        ('%', "\\%"),
+        ('$', "\\$"),
+        ('#', "\\#"),
+        ('_', "\\_"),
+        ('{', "$\\{$"),
+        ('}', "$\\}$"),
+        ('~', "\\~"),
+        // TODO
+        ('^', ""),
+        ('\\', "\\backslash"),
+    ];
+    let mut output = String::new();
+
+    for ch in seq.chars() {
+        let mut escaped = false;
+        for esc_ch in escaped_ch.iter() {
+            if esc_ch.0 == ch {
+                escaped = true;
+                output.push_str(esc_ch.1);
+            }
+        }
+        if !escaped {
+            output.push(ch);
+        }
+    }
+    output
 }
